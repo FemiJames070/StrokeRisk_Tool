8cf112153cd7cada1b679594107f7e39a12bb44f
Femi James <femijames070@gmail.com>
2025-08-11 17:38:59 -0600

    Update 09_Patient_Profile.py

--- pages/09_Patient_Profile.py
+++ pages/09_Patient_Profile.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 
 def client_profile_page():
     """
@@ -428,7 +429,7 @@ def client_profile_page():
     """
     # Use st.components.v1.html to embed the full HTML content
     # Set height to a reasonable value and enable scrolling.
-    st.components.v1.html(full_html_content, height=2800, scrolling=False) # Increased height slightly for footer
+    st.components.v1.html(full_html_content, height=1350, width=1500, scrolling=False) # Increased height slightly for footer
 
 # Call the function to render the page.
 client_profile_page()
