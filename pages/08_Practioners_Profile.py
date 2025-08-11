b40c82b01af95b6b693854973fb10ac66551e1a3
Femi James <femijames070@gmail.com>
2025-08-11 17:38:14 -0600

    Update 08_Practioners_Profile.py

--- pages/08_Practioners_Profile.py
+++ pages/08_Practioners_Profile.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 
 def practitioner_profile_page():
     """
@@ -392,7 +393,7 @@ def practitioner_profile_page():
     </html>
     """
     # Set height to a fixed value (increased from 1800 to 2500) and scrolling to False to prevent internal scrollbar.
-    st.components.v1.html(full_html_content, height=2500, scrolling=True) # Increased height and enabled scrolling
+    st.components.v1.html(full_html_content, height=1350, width=1500, scrolling=True) # Increased height and enabled scrolling
 
 # Call the function to render the page.
 practitioner_profile_page()
